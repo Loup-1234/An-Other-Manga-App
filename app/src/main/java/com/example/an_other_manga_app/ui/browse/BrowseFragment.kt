24736f5ae612c77935148f385fdd6220b561f660
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-02-26 14:39:17 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/ui/browse/BrowseFragment.kt
+++ app/src/main/java/com/example/an_other_manga_app/ui/browse/BrowseFragment.kt
@@ -13,8 +13,6 @@ class BrowseFragment : Fragment() {
 
     private var _binding: FragmentBrowseBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
--- app/src/main/java/com/example/an_other_manga_app/ui/library/LlibraryFragment.kt
+++ app/src/main/java/com/example/an_other_manga_app/ui/library/LlibraryFragment.kt
@@ -13,8 +13,6 @@ class LibraryFragment : Fragment() {
 
     private var _binding: FragmentLibraryBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,14 +20,14 @@ class LibraryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[LibraryViewModel::class.java]
+        val homeViewModel =
+            ViewModelProvider(this).get(LibraryViewModel::class.java)
 
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textLibrary
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
