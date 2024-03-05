db3d13c98409a525f47e95d8e566becbac726684
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-05 11:28:09 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
+++ app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
+
+data class BottomNavItem(val iconResId: Int, val label: String)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +20,14 @@ class MainActivity : ComponentActivity() {
             BottomAppBar {
                 Row {
                     var selectedItem by remember { mutableIntStateOf(0) }
-                    val items = listOf("Songs", "Artists", "Playlists")
+                    val items = listOf(
+                        BottomNavItem(R.drawable.ic_library_24dp, "Library"),
+                        BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
+                    )
                     items.forEachIndexed { index, item ->
                         NavigationBarItem(
-                            icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                            label = { Text(item) },
+                            icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
+                            label = { Text(item.label) },
                             selected = selectedItem == index,
                             onClick = { selectedItem = index }
                         )
--- app/src/main/java/com/example/an_other_manga_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/an_other_manga_app/ui/theme/Theme.kt
@@ -1,7 +1,7 @@
 package com.example.an_other_manga_app.ui.theme
 
-import android.app.Activity
 import android.os.Build
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -25,22 +25,11 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun AnOtherMangaAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -56,7 +45,7 @@ fun AnOtherMangaAppTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
+            val window = (view.context as ComponentActivity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
