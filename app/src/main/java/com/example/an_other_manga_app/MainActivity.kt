cd8b1fb32d0d5851cee2fa5b5a93c1987e818026
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-05 14:05:38 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
+++ app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
@@ -1,56 +1,64 @@
 package com.example.an_other_manga_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import com.example.an_other_manga_app.ui.theme.AnOtherMangaAppTheme
 
 data class BottomNavItem(val iconResId: Int, val label: String)
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
+            AnOtherMangaAppTheme {
+                val navController = rememberNavController()
 
-            Box(modifier = Modifier.fillMaxSize()) {
-                AppNavigation(navController = navController)
+                var selectedItem by remember { mutableIntStateOf(0) }
+                val items = listOf(
+                    BottomNavItem(R.drawable.ic_library_24dp, "Library"),
+                    BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
+                )
 
-                BottomAppBar {
-                    var selectedItem by remember { mutableIntStateOf(0) }
-                    val items = listOf(
-                        BottomNavItem(R.drawable.ic_library_24dp, "Library"),
-                        BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
-                    )
-                    items.forEachIndexed { index, item ->
-                        NavigationBarItem(
-                            icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
-                            label = { Text(item.label) },
-                            selected = selectedItem == index,
-                            onClick = {
-                                selectedItem = index
-                                if (index == 0) {
-                                    navController.navigate("library")
-                                } else {
-                                    navController.navigate("browse")
-                                }
+                Scaffold(
+                    content = { AppNavigation(navController = navController) },
+                    bottomBar = {
+                        BottomAppBar(
+                            contentPadding = PaddingValues(16.dp)
+                        ) {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
+                                    label = { Text(item.label) },
+                                    selected = selectedItem == index,
+                                    onClick = {
+                                        selectedItem = index
+                                        if (index == 0) {
+                                            navController.navigate("library")
+                                        } else {
+                                            navController.navigate("browse")
+                                        }
+                                    }
+                                )
                             }
-                        )
+                        }
                     }
-                }
+                )
             }
         }
     }
--- app/src/main/java/com/example/an_other_manga_app/fragment/LibraryFragment.kt
+++ app/src/main/java/com/example/an_other_manga_app/fragment/LibraryFragment.kt
@@ -9,25 +9,40 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
-class LibraryFragment {
+object LibraryFragment {
     @Composable
-    fun ElevatedCard(image: Painter, title: String, description: String) {
+    fun ElevatedCard(image: Int, title: String, description: String) {
         ElevatedCard(
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 6.dp
-            ),
-            modifier = Modifier
-                .size(width = 240.dp, height = 100.dp)
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+            modifier = Modifier.size(width = 240.dp, height = 100.dp)
         ) {
             Column {
-                Image(painter = image, contentDescription = "Image")
+                Image(
+                    painter = painterResource(id = image),
+                    contentDescription = "Image"
+                )
                 Text(text = title, modifier = Modifier.padding(16.dp), textAlign = TextAlign.Start)
                 Text(text = description, modifier = Modifier.padding(16.dp), textAlign = TextAlign.Start)
             }
         }
     }
+
+    @Composable
+    fun MultipleElevatedCards(cardItems: List<CardItem>) {
+        Column {
+            cardItems.forEach { item ->
+                ElevatedCard(
+                    image = item.image,
+                    title = item.title,
+                    description = item.description
+                )
+            }
+        }
+    }
+
+    data class CardItem(val image: Int, val title: String, val description: String)
 }
