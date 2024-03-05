8dff3ec81667007b0ed25ad3fb61adf1ee658278
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-05 20:45:21 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
+++ app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.an_other_manga_app
 
+// Importations nécessaires pour l'application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,36 +19,56 @@ import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.rememberNavController
 import com.example.an_other_manga_app.ui.theme.AnOtherMangaAppTheme
 
+// Classe de données pour représenter un élément de la barre de navigation inférieure
+// Chaque élément a une icône et une étiquette
 data class BottomNavItem(val iconResId: Int, val label: String)
 
+// Activité principale de l'application
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Utilisation du thème défini pour l'application
             AnOtherMangaAppTheme {
+                // Création d'un NavController pour gérer la navigation dans l'application
                 val navController = rememberNavController()
 
+                // Liste des éléments de la barre de navigation inférieure
                 val items = remember {
                     listOf(
                         BottomNavItem(R.drawable.ic_library_24dp, "Library"),
                         BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
                     )
                 }
+
+                // État pour suivre l'élément sélectionné dans la barre de navigation inférieure
                 var selectedItem by remember { mutableIntStateOf(0) }
 
+                // Box qui remplit toute la taille de l'écran
                 Box(modifier = Modifier.fillMaxSize()) {
+                    // Navigation de l'application
                     AppNavigation(navController = navController)
 
+                    // Barre de navigation inférieure
                     BottomAppBar {
+                        // Pour chaque élément dans la liste des éléments
                         items.forEachIndexed { index, item ->
+                            // Création d'un élément de navigation
                             NavigationBarItem(
+                                // Icône pour l'élément
                                 icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
+                                // Étiquette pour l'élément
                                 label = { Text(item.label) },
+                                // Vérifie si l'élément est actuellement sélectionné
                                 selected = selectedItem == index,
+                                // Action à effectuer lorsqu'on clique sur l'élément
                                 onClick = {
-                                    if (selectedItem != index) {
+                                    if (selectedItem != index) { // Si l'élément sélectionné n'est pas l'élément actuel
+                                        // Met à jour l'élément sélectionné
                                         selectedItem = index
+                                        // Détermine la destination en fonction de l'index
                                         val destination = if (index == 0) "library" else "browse"
+                                        // Navigue vers la destination
                                         navController.navigate(destination)
                                     }
                                 }
--- app/src/main/java/com/example/an_other_manga_app/Navigation.kt
+++ app/src/main/java/com/example/an_other_manga_app/Navigation.kt
@@ -1,5 +1,6 @@
 package com.example.an_other_manga_app
 
+// Importations nécessaires pour l'application
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -7,10 +8,14 @@ import androidx.navigation.compose.composable
 import com.example.an_other_manga_app.ui.browse.BrowseFragment
 import com.example.an_other_manga_app.ui.library.LibraryFragment
 
+// Fonction Composable pour la navigation dans l'application
 @Composable
 fun AppNavigation(navController: NavHostController) {
+    // Hôte de navigation avec le contrôleur de navigation et la destination de départ
     NavHost(navController, startDestination = "library") {
+        // Destination composable pour la bibliothèque
         composable("library") { LibraryFragment() }
+        // Destination composable pour la navigation
         composable("browse") { BrowseFragment() }
     }
 }
