c23b5135f018270be333cd1b1095fa6dbc2ec4c0
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-05 19:35:07 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
+++ app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.rememberNavController
+import com.example.an_other_manga_app.ui.theme.AnOtherMangaAppTheme
 
 data class BottomNavItem(val iconResId: Int, val label: String)
 
@@ -23,31 +24,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
+            AnOtherMangaAppTheme {
+                val navController = rememberNavController()
 
-            Box(modifier = Modifier.fillMaxSize()) {
-                AppNavigation(navController = navController)
-
-                BottomAppBar {
-                    var selectedItem by remember { mutableIntStateOf(0) }
-                    val items = listOf(
+                val items = remember {
+                    listOf(
                         BottomNavItem(R.drawable.ic_library_24dp, "Library"),
                         BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
                     )
-                    items.forEachIndexed { index, item ->
-                        NavigationBarItem(
-                            icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
-                            label = { Text(item.label) },
-                            selected = selectedItem == index,
-                            onClick = {
-                                selectedItem = index
-                                if (index == 0) {
-                                    navController.navigate("library")
-                                } else {
-                                    navController.navigate("browse")
+                }
+                var selectedItem by remember { mutableIntStateOf(0) }
+
+                Box(modifier = Modifier.fillMaxSize()) {
+                    AppNavigation(navController = navController)
+
+                    BottomAppBar {
+                        items.forEachIndexed { index, item ->
+                            NavigationBarItem(
+                                icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
+                                label = { Text(item.label) },
+                                selected = selectedItem == index,
+                                onClick = {
+                                    selectedItem = index
+                                    val destination = if (index == 0) "library" else "browse"
+                                    navController.navigate(destination)
                                 }
-                            }
-                        )
+                            )
+                        }
                     }
                 }
             }
