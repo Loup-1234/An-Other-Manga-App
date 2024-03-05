5c3786616fd13c85399576b6612203dfe732e2e7
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-05 12:58:08 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
+++ app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.an_other_manga_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import com.example.an_other_manga_app.ui.theme.AnOtherMangaAppTheme
+import androidx.navigation.compose.rememberNavController
 
 data class BottomNavItem(val iconResId: Int, val label: String)
 
@@ -18,22 +24,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AnOtherMangaAppTheme {
+            val navController = rememberNavController()
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                AppNavigation(navController = navController)
+
                 BottomAppBar {
-                    Row {
-                        var selectedItem by remember { mutableIntStateOf(0) }
-                        val items = listOf(
-                            BottomNavItem(R.drawable.ic_library_24dp, "Library"),
-                            BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
+                    var selectedItem by remember { mutableIntStateOf(0) }
+                    val items = listOf(
+                        BottomNavItem(R.drawable.ic_library_24dp, "Library"),
+                        BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
+                    )
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
+                            label = { Text(item.label) },
+                            selected = selectedItem == index,
+                            onClick = {
+                                selectedItem = index
+                                if (index == 0) {
+                                    navController.navigate("library")
+                                } else {
+                                    navController.navigate("browse")
+                                }
+                            }
                         )
-                        items.forEachIndexed { index, item ->
-                            NavigationBarItem(
-                                icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
-                                label = { Text(item.label) },
-                                selected = selectedItem == index,
-                                onClick = { selectedItem = index }
-                            )
-                        }
                     }
                 }
             }
--- app/src/main/java/com/example/an_other_manga_app/fragment/LibraryFragment.kt
+++ app/src/main/java/com/example/an_other_manga_app/fragment/LibraryFragment.kt
@@ -1,4 +1,33 @@
 package com.example.an_other_manga_app.fragment
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
 class LibraryFragment {
+    @Composable
+    fun ElevatedCard(image: Painter, title: String, description: String) {
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            modifier = Modifier
+                .size(width = 240.dp, height = 100.dp)
+        ) {
+            Column {
+                Image(painter = image, contentDescription = "Image")
+                Text(text = title, modifier = Modifier.padding(16.dp), textAlign = TextAlign.Start)
+                Text(text = description, modifier = Modifier.padding(16.dp), textAlign = TextAlign.Start)
+            }
+        }
+    }
 }
