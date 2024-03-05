681cbad0316c420867e59c63f3f8e21e3e972c5e
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-05 11:29:15 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
+++ app/src/main/java/com/example/an_other_manga_app/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.res.painterResource
+import com.example.an_other_manga_app.ui.theme.AnOtherMangaAppTheme
 
 data class BottomNavItem(val iconResId: Int, val label: String)
 
@@ -17,20 +18,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BottomAppBar {
-                Row {
-                    var selectedItem by remember { mutableIntStateOf(0) }
-                    val items = listOf(
-                        BottomNavItem(R.drawable.ic_library_24dp, "Library"),
-                        BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
-                    )
-                    items.forEachIndexed { index, item ->
-                        NavigationBarItem(
-                            icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
-                            label = { Text(item.label) },
-                            selected = selectedItem == index,
-                            onClick = { selectedItem = index }
+            AnOtherMangaAppTheme {
+                BottomAppBar {
+                    Row {
+                        var selectedItem by remember { mutableIntStateOf(0) }
+                        val items = listOf(
+                            BottomNavItem(R.drawable.ic_library_24dp, "Library"),
+                            BottomNavItem(R.drawable.ic_browse_24dp, "Browse")
                         )
+                        items.forEachIndexed { index, item ->
+                            NavigationBarItem(
+                                icon = { Icon(painterResource(id = item.iconResId), contentDescription = item.label) },
+                                label = { Text(item.label) },
+                                selected = selectedItem == index,
+                                onClick = { selectedItem = index }
+                            )
+                        }
                     }
                 }
             }
