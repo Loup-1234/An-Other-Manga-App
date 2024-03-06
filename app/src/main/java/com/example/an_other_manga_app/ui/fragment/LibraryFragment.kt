425b31f7819a19b7ba9f00df574c1657a53d3d81
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-06 18:40:01 +0100

    BAU BAU

--- app/src/main/java/com/example/an_other_manga_app/ui/fragment/LibraryFragment.kt
+++ app/src/main/java/com/example/an_other_manga_app/ui/fragment/LibraryFragment.kt
@@ -3,8 +3,10 @@ package com.example.an_other_manga_app.ui.fragment
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
@@ -14,32 +16,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.an_other_manga_app.ui.theme.AnOtherMangaAppTheme
 
 @Composable
 fun LibraryFragment() {
-    AnOtherMangaAppTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = colorScheme.surface),
-            contentAlignment = Alignment.Center
-        ) {
-            Card(
-                colors = CardDefaults.cardColors(
-                    containerColor = colorScheme.surfaceVariant,
-                ),
+    LazyColumn {
+        items(1000) { index ->
+            Box(
                 modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
-            ) {
-                Text(
-                    text = "Filled Card",
+                    .fillMaxSize()
+                    .background(color = colorScheme.surface),
+                contentAlignment = Alignment.Center ) {
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = colorScheme.surfaceVariant,
+                    ),
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    textAlign = TextAlign.Center,
-                    color = colorScheme.secondary
-                )
+                        .width(350.dp)
+                        .height(450.dp)
+                        .padding(8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.BottomCenter
+                    ) {
+                        Text(
+                            text = "Manga $index",
+                            modifier = Modifier
+                                .padding(16.dp),
+                            textAlign = TextAlign.Center,
+                            color = colorScheme.secondary
+                        )
+                    }
+                }
             }
         }
     }
