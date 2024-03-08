f201388d9d4eaa27e719b78a73ba2242cdbe7701
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-03-08 18:27:18 +0100

    BAU BAU - Why not generating cards every 0.5 sec?

--- app/src/main/java/com/example/an_other_manga_app/ui/fragment/LibraryFragment.kt
+++ app/src/main/java/com/example/an_other_manga_app/ui/fragment/LibraryFragment.kt
@@ -11,11 +11,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -23,15 +27,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.an_other_manga_app.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun LibraryFragment(navController: NavHostController) {
+    val items = generateInfiniteListOfItems()
     Box(modifier = Modifier.padding(bottom = 80.dp)) {
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             contentPadding = PaddingValues(8.dp)
         ) {
-            items(10) { index ->
+            items(items) { index ->
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
@@ -73,4 +79,22 @@ fun LibraryFragment(navController: NavHostController) {
             }
         }
     }
+}
+
+data class Item(val id: Int)
+
+@Composable
+fun generateInfiniteListOfItems(): List<Item> {
+
+    val items = remember { mutableStateListOf<Item>() }
+
+    LaunchedEffect(Unit) {
+        var index = 0
+        while(true) {
+            items.add(Item(index++))
+            delay(500)
+        }
+    }
+
+    return items
 }
